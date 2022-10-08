ed5658640419f7016944ab0376c95bb65efb46e0
Nefi Perez <nefipr@gmail.com>
2022-10-08 15:02:52 +0100

    Connect VSC with GitHub

---
+++ main.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    },
+    {
+        label: "Week2 notes",
+        url: "week2/index.html"
+    },
+    {
+        label: "Week3 notes",
+        url: "week3/index.html"
+    }     
+  ]      
+
+// const unordered_list = document.createElement("li")
+for(let i = 0; i < links.length; i++){
+    document.getElementById("directory").innerHTML+=`<li><a href='${links[i].url}'>${links[i].label}<a/></li>`
+}
+
