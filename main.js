57c52bb7afc1b26c1fc8ce113c6c7b7a5330ca0b
Nefi Perez <nefipr@gmail.com>
2022-10-22 23:55:14 +0100

    Midterm

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week1 Reading Assignment",
       url: "week1/story_editor.html"
     },
     {
-        label: "Week2 notes",
-        url: "week2/index.html"
+        label: "Week2 Reading Assignment",
+        url: "week2/Readings2.html"
     },
     {
-        label: "Week3 notes",
-        url: "week3/index.html"
-    }     
+      label: "Week2 Team Assignment",
+      url: "week2/TeamWK2.html"
+    },
+    {
+        label: "Week3 Reading Assignment",
+        url: "week3/Readings3.html"
+    },
+    {
+      label: "Week3 Team Assignment",
+      url: "week3/TeamWK3.html"
+    },    
+    {
+      label: "Week4 Reading Assignment",
+      url: "week4/Readings4.html"
+    },
+    {
+      label: "Week4 Team Assignment",
+      url: "week4/TeamWK4.html"
+    },    
+    {
+        label: "Week5 Reading Assignment",
+        url: "week5/Readings5.html"
+    },
+    {
+      label: "Week5 Team Assignment",
+      url: "week5/W06team.html"
+    },     
+    {
+        label: "ToDo Assignment",
+        url: "ToDo/ToDo.html"
+    }    
+    
   ]      
 
 // const unordered_list = document.createElement("li")
