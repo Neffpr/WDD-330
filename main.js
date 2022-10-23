9ce6e3e194f8f0754733a9d9e8a7d7d49f7b0872
Nefi Perez Reyes <92007267+Neffpr@users.noreply.github.com>
2022-10-23 02:15:47 +0100

    update index

--- main.js
+++ main.js
@@ -1,49 +1,51 @@
 const links = [
     {
       label: "Week1 Reading Assignment",
-      url: "week1/story_editor.html"
+      url: "Week1/story_editor.html"
     },
     {
         label: "Week2 Reading Assignment",
-        url: "week2/Readings2.html"
+        url: "Week2/Readings2.html"
     },
     {
       label: "Week2 Team Assignment",
-      url: "week2/TeamWK2.html"
+      url: "Week2/TeamWK2.html"
     },
     {
         label: "Week3 Reading Assignment",
-        url: "week3/Readings3.html"
+        url: "Week3/Readings3.html"
     },
     {
       label: "Week3 Team Assignment",
-      url: "week3/TeamWK3.html"
+      url: "Week3/TeamWK3.html"
     },    
     {
       label: "Week4 Reading Assignment",
-      url: "week4/Readings4.html"
+      url: "Week4/Readings4.html"
     },
     {
       label: "Week4 Team Assignment",
-      url: "week4/TeamWK4.html"
+      url: "Week4/TeamWK4.html"
     },    
     {
         label: "Week5 Reading Assignment",
-        url: "week5/Readings5.html"
+        url: "Week5/Readings5.html"
     },
     {
       label: "Week5 Team Assignment",
-      url: "week5/W06team.html"
+      url: "Week5/W06team.html"
     },     
     {
         label: "ToDo Assignment",
         url: "ToDo/ToDo.html"
+    },
+    {
+      label: "ToDo Assignment // 2 Attemptive",
+      url: "ToDo/index.html"
     }    
-    
   ]      
 
 // const unordered_list = document.createElement("li")
 for(let i = 0; i < links.length; i++){
     document.getElementById("directory").innerHTML+=`<li><a href='${links[i].url}'>${links[i].label}<a/></li>`
 }
-
